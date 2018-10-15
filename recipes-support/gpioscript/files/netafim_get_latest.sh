7e852b1acf3c2d1d130ca84f09e477d43906437f
Vladimir Chernin <ran.w@amarel-aes.com>
2018-10-15 16:49:49 +0300

    update script

--- recipes-support/gpioscript/files/netafim_get_latest.sh
+++ recipes-support/gpioscript/files/netafim_get_latest.sh
@@ -1,28 +1,38 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ -z $1 ]; then
-  echo " please provided download dir"
-  exit 1
+  echo " no download dir is specified, using /tmp "
+  DL_DIR="/tmp"
+else
+  DL_DIR=$1
 fi
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_AcdcTile -d $1
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_AnalogIn -d $1
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_AnalogOut -d $1
-#/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_IsmTile -d $1
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_Power -d $1
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_Tiles_Repeatera -d $1
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_RtuTile -d $1
-/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_Tiles_IsmParentTile -d $1
-/bin/get_bundle.py -p NetBeat       -b NetBeat_Installer -d $1
-chmod a+x *.bsx
+
+rm $DL_DIR/*.bsx
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_AcdcTile -d $DL_DIR
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_AnalogIn -d $DL_DIR
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_AnalogOut -d $DL_DIR
+#/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_IsmTile -d $DL_DIR
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_Power -d $DL_DIR
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_Tiles_Repeatera -d $DL_DIR
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_RtuTile -d $DL_DIR
+/bin/get_bundle.py -p NetBeat_Tiles -b NetBeat_Tiles_IsmParentTile -d $DL_DIR
+/bin/get_bundle.py -p NetBeat       -b NetBeat_Installer -d $DL_DIR
+/bin/get_bundle.py -p NetBeat       -b NetBeat_ProductionTests -d $DL_DIR
+chmod a+x $DL_DIR/*.bsx
+rm /usr/sbin/*.bsx
+
+cp $DL_DIR/*.bsx /usr/sbin/
 #create tile installer ( STM32 Programmer ) 
 if [ -f /bin/tile_installer.tar.gz ]
 then 
   # move all binaries to installer and create and installer
   # this installer performes STM32 JTAG programming out-of-the-box
-  tar -zxvf /bin/tile_installer.tar.gz -C $1
-  cp $1/*.bin $1/tile_installer/payload/
-  cd $1/tile_installer
+  tar -zxvf /bin/tile_installer.tar.gz -C $DL_DIR
+  cp $DL_DIR/*.bin $DL_DIR/tile_installer/payload/
+  cd $DL_DIR/tile_installer
   ./build
-  cp install_tile /bin
+  cp $DL_DIR/tile_installer/install_tile /bin
   cd ../
 fi
+mkdir -p production_tests
+tar -zxvf $DL_DIR/production_tests.tar.gz -C $DL_DIR/production_tests > /dev/null
